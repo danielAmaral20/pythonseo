d44e19b353b4d41cebd330db0505ac21a7c04698
Daniel Amaral <60074138+danielAmaral20@users.noreply.github.com>
2024-05-02 19:25:26 -0300

    Update sitemap-check.py

--- sitemap-check.py
+++ sitemap-check.py
@@ -5,6 +5,12 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 def check_urls(urls):
+    try:
+        response = requests.get(urls[0])
+        main_status_code = response.status_code
+    except Exception as e:
+        main_status_code = str(e)
+
     results = []
     for url in urls:
         try:
@@ -33,7 +39,7 @@ def check_urls(urls):
             results.append((url, status_code, has_noindex_tag, canonical_match, canonical_url_diff))
         except Exception as e:
             results.append((url, str(e), False, False, ""))
-    return results
+    return main_status_code, results
 
 def parse_sitemap(sitemap_url):
     response = requests.get(sitemap_url)
@@ -50,13 +56,14 @@ def main():
         if sitemap_url:
             try:
                 urls = parse_sitemap(sitemap_url)
-                results = check_urls(urls)
+                main_status_code, results = check_urls(urls)
                 df = pd.DataFrame(results, columns=["URL", "Status Code", "Possui Noindex", "Canonical Correspondente", "Canonical Diferente"])
                 
                 # Mapeando valores booleanos para strings descritivas
                 df["Possui Noindex"] = df["Possui Noindex"].map({True: "Possui Noindex", False: "Não possui Noindex"})
                 df["Canonical Correspondente"] = df["Canonical Correspondente"].map({True: "Correspondente", False: "Diferente"})
                 
+                st.write("Status Code da URL principal:", main_status_code)
                 st.write(df)
             except Exception as e:
                 st.error(f"Erro ao processar o sitemap: {e}")
