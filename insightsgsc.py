ef78a2a7de8c516bc00b13a3ca43bd5bf86b7282
Daniel Amaral <60074138+danielAmaral20@users.noreply.github.com>
2024-10-09 20:34:33 -0300

    Update insightsgsc.py

--- insightsgsc.py
+++ insightsgsc.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+import httplib2
 
 # Função para autenticação e conexão com o Google Search Console
 def authenticate_gsc(client_id, client_secret, oauth_scope, redirect_uri):
@@ -15,21 +16,10 @@ def authenticate_gsc(client_id, client_secret, oauth_scope, redirect_uri):
         }
     }, [oauth_scope])
     
-    # Exibe o link de autenticação manual no Streamlit
-    auth_url, _ = flow.authorization_url(prompt='consent')
-    st.write("Autentique-se visitando o seguinte link:")
-    st.write(auth_url)
-
-    # Solicita o código de autenticação
-    auth_code = st.text_input("Digite o código de autenticação:")
-
-    if auth_code:
-        # Faz o fetch do token usando o código de autenticação
-        flow.fetch_token(code=auth_code)
-        service = build('searchconsole', 'v1', credentials=flow.credentials)
-        return service
-
-    return None
+    credentials = flow.run_local_server(port=0)
+    service = build('searchconsole', 'v1', credentials=credentials)
+    
+    return service
 
 # Função para buscar a lista de sites conectados
 def get_site_list(service):
@@ -47,15 +37,13 @@ REDIRECT_URI = "https://seodatainsights.streamlit.app/"
 st.title("Google Search Console Analyzer")
 
 # Autenticação e conexão com a API
-service = authenticate_gsc(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+with st.spinner("Autenticando com o Google Search Console..."):
+    service = authenticate_gsc(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
 
-if service:
-    # Obtenção da lista de sites
-    sites = get_site_list(service)
+# Obtenção da lista de sites
+sites = get_site_list(service)
 
-    # Dropdown para seleção de site
-    selected_site = st.selectbox("Selecione um site para análise:", sites)
+# Dropdown para seleção de site
+selected_site = st.selectbox("Selecione um site para análise:", sites)
 
-    st.write(f"Você selecionou: {selected_site}")
-else:
-    st.write("Aguardando autenticação...")
+st.write(f"Você selecionou: {selected_site}")
